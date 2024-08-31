a5560dfe91f41f0618dc6a2cdff3c7c16949d626
madmaxtoastflytiger <102840473+madmaxtoastflytiger@users.noreply.github.com>
2024-08-31 16:22:08 -0400

    Finished 1/4th

--- SQLTasks Tier 1.sql
+++ SQLTasks Tier 1.sql
@@ -34,8 +34,14 @@ exploring the data, and getting acquainted with the 3 tables. */
 /* Q1: Some of the facilities charge a fee to members, but some do not.
 Write a SQL query to produce a list of the names of the facilities that do. */
 
+SELECT *
+FROM `Facilities`
+WHERE membercost !=0;
+
 
 /* Q2: How many facilities do not charge a fee to members? */
+
+5 facilities (Tennis Court 1, Tennis Court 2, Massage Room 1, Massage Room 2, Squash Court)
 
 
 /* Q3: Write an SQL query to show a list of facilities that charge a fee to members,
@@ -43,9 +49,18 @@ where the fee is less than 20% of the facility's monthly maintenance cost.
 Return the facid, facility name, member cost, and monthly maintenance of the
 facilities in question. */
 
+SELECT facid, name, membercost, monthlymaintenance
+FROM `Facilities`
+WHERE membercost !=0 
+    AND membercost < monthlymaintenance * 0.2;
+
 
 /* Q4: Write an SQL query to retrieve the details of facilities with ID 1 and 5.
 Try writing the query without using the OR operator. */
+
+SELECT facid, name, membercost, monthlymaintenance
+FROM `Facilities`
+WHERE facid IN (1,5);
 
 
 /* Q5: Produce a list of facilities, with each labelled as
