0338ca89f5445c632b002e2171f9151920cc1c2f
madmaxtoastflytiger <102840473+madmaxtoastflytiger@users.noreply.github.com>
2024-09-02 14:07:57 -0400

    finish all but Q10

--- SQLTasks Tier 1.sql
+++ SQLTasks Tier 1.sql
@@ -140,8 +140,10 @@ SELECT
     m.surname, -- still having issue concatenating columns together
     m.firstname,
     CASE 
-        WHEN b.memid = 0 THEN f.guestcost * b.slots  -- Check guest cost if memid is 0
-        ELSE f.membercost * b.slots  -- Check member cost if memid is not 0
+        WHEN b.memid = 0 -- Check guest cost if memid is 0
+            THEN f.guestcost * b.slots  
+        ELSE 
+            f.membercost * b.slots
     END AS total_cost  -- Calculate total cost
 FROM Bookings AS b -- bookings asd both info to connect all 3 tables together
 LEFT JOIN Facilities AS f 
@@ -151,8 +153,10 @@ LEFT JOIN Members as m
 WHERE 
     b.starttime LIKE '2012-09-14%'
     AND CASE 
-        WHEN b.memid = 0 THEN f.guestcost * b.slots  -- Check guest cost if memid is 0
-        ELSE f.membercost * b.slots  -- Check member cost if memid is not 0
+        WHEN b.memid = 0   -- Check guest cost if memid is 0
+            THEN f.guestcost * b.slots  
+        ELSE 
+            f.membercost * b.slots
     END >= 30  -- Filter for total costs above $30
 ORDER BY 
     total_cost DESC;  -- Sort by total cost from highest to lowest
@@ -163,18 +167,20 @@ ORDER BY
 /* Q9: This time, produce the same result as in Q8, but using a subquery. */
 
 SELECT 
-    subquery.fac_name,
-    subquery.surname,
-    subquery.firstname,
-    subquery.total_cost
+    subq.fac_name,
+    subq.surname,
+    subq.firstname,
+    subq.total_cost
 FROM (
     SELECT 
         f.name AS fac_name,
         m.surname,
         m.firstname,
         CASE 
-            WHEN b.memid = 0 THEN f.guestcost * b.slots  -- Check guest cost if memid is 0
-            ELSE f.membercost * b.slots  -- Check member cost if memid is not 0
+            WHEN b.memid = 0 
+                THEN f.guestcost * b.slots
+            ELSE
+                f.membercost * b.slots
         END AS total_cost  -- Calculate total cost
     FROM Bookings AS b
     LEFT JOIN Facilities AS f 
@@ -183,11 +189,11 @@ FROM (
         ON b.memid = m.memid
     WHERE 
         b.starttime LIKE '2012-09-14%'
-) AS subquery
+) AS subq
 WHERE 
-    subquery.total_cost >= 30  -- Filter for total costs above $30
+    subq.total_cost >= 30  -- Filter for total costs above $30
 ORDER BY 
-    subquery.total_cost DESC;  -- Sort by total cost from highest to lowest
+    subq.total_cost DESC;  -- Sort by total cost from highest to lowest
 
 
 
@@ -212,11 +218,59 @@ QUESTIONS:
 The output of facility name and total revenue, sorted by revenue. Remember
 that there's a different cost for guests and members! */
 
+
+
+
+
+
 /* Q11: Produce a report of members and who recommended them in alphabetic surname,firstname order */
+
+SELECT 
+    m.surname, 
+    m.firstname, 
+    m.recommendedby AS recomender_id, 
+    recommender.surname AS recomender_surname, 
+    recommender.firstname AS recomender_firstname
+FROM Members AS m
+LEFT JOIN Members AS recommender -- duplicate members table as recommender 
+    ON m.recommendedby = recommender.memid
+WHERE m.recommendedby != 0
+ORDER BY m.surname, m.firstname;
+
+
 
 
 /* Q12: Find the facilities with their usage by member, but not guests */
 
+SELECT
+    f.name AS fac_nam,
+    subq.facid, 
+    COUNT( subq.memid ) AS member_usage
+FROM (
+    -- subq, return real members and which facility id
+    SELECT 
+        facid,
+        memid
+    FROM Bookings
+    WHERE memid !=0
+) AS subq
+LEFT JOIN Facilities AS f 
+    USING(facid)
+GROUP BY subq.facid
+ORDER BY member_usage DESC;
+
+
+
 
 /* Q13: Find the facilities usage by month, but not guests */
 
+SELECT 
+    MONTH( b.starttime ) AS months, -- extract only the month
+    f.name AS fac_name,
+    COUNT(b.memid) AS fac_usage -- number of member usage
+FROM Bookings AS b
+LEFT JOIN Facilities AS f -- this is to get the facilities name 
+    USING (facid)
+WHERE 
+    memid !=0
+GROUP BY MONTH( b.starttime ), f.name
